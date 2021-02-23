953dc14de6196d6c5891d661888e60f1b58025fe
Mayank Kheterpal <77799836+MayankKheterpal@users.noreply.github.com>
2021-02-23 14:52:13 -0600

    created

---
+++ DragAndDropBoxes/script.js
@@ -0,0 +1,45 @@
+const boxes = document.querySelectorAll('.box')
+const fill = document.querySelector('.fill')
+
+fill.addEventListener('dragstart',dragStart)
+fill.addEventListener('dragend',dragEnd)
+
+for(const box of boxes){
+
+    box.addEventListener ('dragover',dragOver)
+    box.addEventListener ('dragenter',dragEnter)
+    box.addEventListener ('dragleave',dragLeave)
+    box.addEventListener ('drop',dragDrop)
+}
+
+function dragStart(){
+    //this.className += ' hold'
+   fill.classList.add('hold')
+setTimeout(()=>
+   this.className = 'invisible', 0)
+
+}
+function dragEnd(){
+    this.className ='fill'
+    
+}
+function dragOver(e){
+    e.preventDefault()
+    //this.className += ' hovered'
+    
+}
+function dragEnter(e){
+    
+    e.preventDefault()
+    this.className += ' hovered'
+}
+function dragLeave(){
+    this.className = 'box'
+    
+}
+function dragDrop(){
+    this.className = 'box'
+    this.append(fill)
+
+    
+}
